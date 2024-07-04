9c114a8c98a07d4d350804fe3e33f9416b8dc0e3
Fred Menge <mengeprofissional@gmail.com>
2024-07-04 14:34:49 -0300

    Testes de manipulação de DOM com TypeScript PURO

---
+++ src/dom.js
@@ -0,0 +1,8 @@
+"use strict";
+const nameInput = document.getElementById('name');
+const form = document.getElementById('form');
+const sendButton = document.getElementById('btn-send');
+sendButton === null || sendButton === void 0 ? void 0 : sendButton.addEventListener('click', (e) => {
+});
+//Embora o TypeScript tenha diversas funções, para o front-end em diversos casos, como na construção de uma landing page
+//o trabalho pode ser demais para um resultado simples, o TypeScript tem uma utilização melhor para o backend
---
+++ src/dom.ts
@@ -0,0 +1,11 @@
+const nameInput = document.getElementById('name');
+const form = document.getElementById('form');
+const sendButton = document.getElementById('btn-send');
+
+sendButton?.addEventListener('click', (e: MouseEvent) => {
+
+});
+
+//Embora o TypeScript tenha diversas funções, para o front-end em diversos casos, como na construção de uma landing page
+//o trabalho pode ser demais para um resultado simples, o TypeScript tem uma utilização melhor para o backend
+
